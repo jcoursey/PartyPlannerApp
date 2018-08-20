09e0578911ec73d6a8fc8243c0cf5652235167ba
Jordan Coursey <jcoursey@ucsd.edu>
2018-08-19 17:02:03 -0700

    Initial Transfer
    transferred file from local folder to github
    
    Co-Authored-By: Justin Lee <paperkatt@users.noreply.github.com>

---
+++ app/src/androidTest/java/com/apockestafe/team19/MainActivityTest.java
@@ -0,0 +1,37 @@
+package com.apockestafe.team19;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by nsallaire on 11/28/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void testAddEvent() {
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.title)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSettingsButton() {
+        onView(withId(R.id.settingsButton)).perform(click());
+        onView(withId(R.id.firstNameEditText)).check(matches(isDisplayed()));
+    }
+}
